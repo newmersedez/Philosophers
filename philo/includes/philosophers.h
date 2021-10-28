05007b23909a542d1d95ad00fe0406652f38c011
newmersedez <trishkk2001@gmail.com>
2021-10-28 22:46:41 +0300

    started philo v2

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -1,6 +1,24 @@
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <memory.h>
+# include <unistd.h>
 
+typedef struct s_philosopher
+{
+
+}	t_philosopher;
+
+typedef struct s_info
+{
+	size_t			philo_num;
+	size_t			must_eat;
+	__useconds_t	time_to_die;
+	__useconds_t	time_to_eat;
+	__useconds_t	time_to_sleep;
+}	t_info;
 
 #endif
---
+++ philo/sources/philosophers.c
@@ -0,0 +1,8 @@
+#include "../includes/philosophers.h"
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return (0);
+}
