06666cf90928840888c9af3964a07efe0a23dac4
newmersedez <trishkk2001@gmail.com>
2022-01-12 01:38:56 +0300

    started bonus

---
+++ philo_bonus/sources/philo_bonus.c
@@ -0,0 +1,14 @@
+#include "../includes/philo_bonus.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    t_table table_info;
+
+    if (try_parse_args(argc, argv, &table_info) != 0)
+	{
+		printf("ti daun\n");
+		return (1);
+	}
+	return (0);
+}
