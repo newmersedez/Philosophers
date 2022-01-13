a55e7c3012c94ba9a0f348d4f869fc42b6737cbf
Lynderly Orphan <lorphan@ox-q5.msk.21-school.ru>
2022-01-13 22:59:18 +0300

    start remains

--- philo/sources/philo_start.c
+++ philo/sources/philo_start.c
@@ -6,14 +6,26 @@
 /*   By: lorphan <lorphan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 18:59:02 by lorphan           #+#    #+#             */
-/*   Updated: 2022/01/13 22:09:33 by lorphan          ###   ########.fr       */
+/*   Updated: 2022/01/13 22:58:49 by lorphan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+static int philo_terminator(t_philo philo)
+{
+	(void)philo;
+}
+
+static int	try_start_threads(t_table *table)
+{
+	(void)table;
+}
+
 void	start_philosophers(t_table *table_info)
 {
 	if (!table_info)
+		return ;
+	if (!try_start_threads(table_info))
 		return ;
 }
