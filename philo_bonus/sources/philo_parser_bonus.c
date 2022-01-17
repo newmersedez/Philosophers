75f19fb2a442710a8ef780c9a3bab196aa3a49ff
newmersedez <trishkk2001@gmail.com>
2022-01-18 02:12:24 +0300

    fixed seg

--- philo_bonus/sources/philo_parser_bonus.c
+++ philo_bonus/sources/philo_parser_bonus.c
@@ -18,14 +18,21 @@ static int check_args(int argc, char *argv[])
 
 static int try_init_semaphores(t_table *table_info)
 {
+	sem_unlink("forks");
 	table_info->forks_sem = sem_open("forks", O_CREAT, 0644, table_info->num_of_philos);
 	if (table_info->forks_sem == SEM_FAILED)
 		return (FALSE);
+	sem_unlink("death");
 	table_info->death_sem = sem_open("death", O_CREAT, 0644, 1);
 	if (table_info->death_sem == SEM_FAILED)
 		return (FALSE);
+	sem_unlink("message");
 	table_info->msg_sem = sem_open("message", O_CREAT, 0644, 1);
 	if (table_info->msg_sem == SEM_FAILED)
+		return (FALSE);
+	sem_unlink("eating_table");
+	table_info->eating_sem = sem_open("eating_table", O_CREAT, 0644, 1);
+	if (table_info->eating_sem == SEM_FAILED)
 		return (FALSE);
 	return (TRUE);
 }
@@ -43,6 +50,7 @@ static int try_init_philosophers(t_table *table_info)
 		table_info->philos[i] = (t_philo *)malloc(sizeof(t_philo));
 		if (!table_info->philos[i])
 			return (FALSE);
+		sem_unlink("eating");
 		table_info->philos[i]->eating_sem = sem_open("eating", O_CREAT, 0644, 1);
 		if (table_info->philos[i]->eating_sem == SEM_FAILED)
 			return (FALSE);
@@ -56,10 +64,13 @@ static int try_init_philosophers(t_table *table_info)
 
 int	try_parse_args(int argc, char *argv[], t_table *table_info)
 {
+	(void)argc;
+	(void)argv;
+	(void)table_info;
 	if (!table_info)
 		return (FALSE);
 	if (argc != 5 && argc != 6)
-		return (1);
+		return (FALSE);
 	if (!check_args(argc, argv))
 		return (1);
 	table_info->num_of_philos = ft_atoi(argv[1]);
